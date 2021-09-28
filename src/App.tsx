dc64cc868be66652144fa7c6f9206dc89e3c548d
Marshall Freed <marshallfreed@gmail.com>
2021-09-27 19:15:59 -0700

    stub in react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <Switch>
+                {/* If the current URL is /about, this route is rendered
+              while the rest are ignored */}
+                <Route path="/about">{/* <AboutMe /> */}</Route>
+
+                <Route path="/about:website">{/* <AboutWebsite /> */}</Route>
+
+                <Route path="/contact">{/* <Contact /> */}</Route>
+
+                {/* If none of the previous routes render anything,
+              this route acts as a fallback.
+  
+              Important: A route with path="/" will *always* match
+              the URL because all URLs begin with a /. So that's
+              why we put this one last of all */}
+                <Route path="/">{/* <Home /> */}</Route>
+            </Switch>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
-import { StyleReset } from 'atomize';
+import { StyleReset } from "atomize";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const debug = process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
 
@@ -15,10 +16,12 @@ ReactDOM.render(
     <React.StrictMode>
         <StyletronProvider value={engine} debug={debug} debugAfterHydration>
             <StyleReset />
-            <App />
+            <Router>
+                <App />
+            </Router>
         </StyletronProvider>
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
