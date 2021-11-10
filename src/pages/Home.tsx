48ae903500f11c8bd734606a8615fe1c53b0add2
Marshall Freed <marshallfreed@gmail.com>
2021-11-09 19:02:24 -0800

    add nvmrc, playing around with homepage styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Container, Grid, Header, Image, Segment } from "semantic-ui-react";
+import { Container, Grid, Header, Image, List, Segment } from "semantic-ui-react";
 import ReactImage from "../assets/logo512.png";
 import ExtensisLogo from "../assets/Extensis_logo.jpg";
 
 const Home: React.FC = () => {
     return (
-        <Container style={{ marginTop: "7em" }}>
-            <Segment vertical>
-                <Grid columns="equal" stackable>
+        <>
+            <Segment vertical style={{ marginTop: "3em" }}>
+                <Grid container stackable columns="equal">
                     <Grid.Row verticalAlign="middle">
                         <Grid.Column floated="left">
                             <Header style={{ fontSize: "3em" }}>Hi, I'm Marshall Freed, a Software Engineer</Header>
@@ -23,23 +23,59 @@ const Home: React.FC = () => {
                 <Grid celled="internally" columns="equal" stackable>
                     <Grid.Row>
                         <Grid.Column floated="left">
-                            <Header as="h1">
-                                <Image size="massive" floated="left" src={ExtensisLogo} />
-                                Extensis
-                                <Header.Subheader>Software Engineer</Header.Subheader>
-                            </Header>
+                            <Grid>
+                                <Grid.Row style={{ paddingBottom: 0, paddingLeft: "1em" }}>
+                                    <Header as="h1" style={{ width: "100%" }}>
+                                        <Image size="massive" floated="left" src={ExtensisLogo} />
+                                        Extensis
+                                        <Header.Subheader>Software Engineer</Header.Subheader>
+                                    </Header>
+                                </Grid.Row>
+                                <Grid.Row style={{ paddingTop: 0 }}>
+                                    <Container text>
+                                        <List bulleted>
+                                            <List.Item>
+                                                Something something something something Something something something
+                                                something
+                                            </List.Item>
+                                            <List.Item>Something something something something</List.Item>
+                                            <List.Item>Something something something something</List.Item>
+                                            <List.Item>Something something something something</List.Item>
+                                        </List>
+                                    </Container>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                         <Grid.Column floated="left">
-                            <Header as="h1">
-                                <Image size="massive" floated="left" src={ExtensisLogo} />
-                                Extensis
-                                <Header.Subheader>Software Engineer In Test</Header.Subheader>
-                            </Header>
+                            <Grid>
+                                <Grid.Row style={{ paddingBottom: 0, paddingLeft: "1em" }}>
+                                    <Header as="h1" style={{ width: "100%" }}>
+                                        <Image size="massive" floated="left" src={ExtensisLogo} />
+                                        Extensis
+                                        <Header.Subheader>Software Engineer In Test</Header.Subheader>
+                                    </Header>
+                                </Grid.Row>
+                                <Grid.Row style={{ paddingTop: 0 }}>
+                                    <Container text>
+                                        <List bulleted>
+                                            <List.Item>
+                                                Something something something something Something something something
+                                                something
+                                            </List.Item>
+                                            <List.Item>Something something something something</List.Item>
+                                            <List.Item>Something something something something</List.Item>
+                                            <List.Item>Something something something something</List.Item>
+                                        </List>
+                                    </Container>
+                                </Grid.Row>
+                            </Grid>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </Segment>
-        </Container>
+
+            <Segment vertical></Segment>
+        </>
     );
 };
 
