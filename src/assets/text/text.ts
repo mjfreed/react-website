5852f5d698c18bf43b9dd2eac54bd976bd77cbf4
Marshall Freed <marshallfreed@gmail.com>
2022-01-25 18:52:28 -0800

    super rough about me page styling

---
+++ src/assets/text/text.ts
@@ -0,0 +1,7 @@
+export const dummyText = `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et porta sapien, nec molestie nisl. Cras ornare,
+    nibh at placerat fermentum, ante sapien auctor quam, sit amet vulputate velit sapien nec ex. Duis iaculis eget
+    mauris vel ornare. Nulla laoreet turpis sapien, sed ornare libero dictum aliquam. Quisque consequat interdum
+    tortor nec aliquet. Cras eget ex eros. Suspendisse venenatis egestas felis, nec efficitur turpis iaculis ut.
+    Aliquam erat volutpat. In lobortis sagittis congue. Suspendisse ultrices nibh quis libero placerat, nec interdum tellus mattis.
+`;
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,11 +1,44 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Grid, Header, Image } from "semantic-ui-react";
+import ReactImage from "../assets/logo512.png";
+import { dummyText } from "../assets/text/text";
 
 const AboutMe: React.FC = () => {
     return (
-        <Container text style={{ marginTop: "7em" }}>
-            <p>About Me PlaceHolder</p>
-        </Container>
+        <Grid style={{ marginTop: "7em" }}>
+            <Grid.Row columns="equal">
+                <Grid.Column>
+                    <Header style={{ fontSize: "3em" }} textAlign="center">
+                        In the workplace...
+                    </Header>
+                    <Container text style={{ fontSize: "1.5em" }}>
+                        {dummyText}
+                    </Container>
+                </Grid.Column>
+                <Grid.Column
+                    verticalAlign="middle"
+                    style={{ justifyContent: "center", alignItems: "center", display: "flex" }}
+                >
+                    <Image src={ReactImage} size="medium" />
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns="equal">
+                <Grid.Column
+                    verticalAlign="middle"
+                    style={{ justifyContent: "center", alignItems: "center", display: "flex" }}
+                >
+                    <Image src={ReactImage} size="medium" />
+                </Grid.Column>
+                <Grid.Column>
+                    <Header style={{ fontSize: "3em" }} textAlign="center">
+                        Outside the workplace...
+                    </Header>
+                    <Container text style={{ fontSize: "1.5em" }}>
+                        {dummyText}
+                    </Container>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     );
 };
 
