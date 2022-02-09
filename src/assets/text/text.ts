10ff3b5b022e2217ba61fd151ec6d25fb56efe22
Marshall Freed <marshallfreed@gmail.com>
2022-02-08 18:56:34 -0800

    add remainder of quick about website WIP

--- src/assets/text/text.ts
+++ src/assets/text/text.ts
@@ -1,6 +1,6 @@
 export const dummyText: string = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisi lacus, pellentesque nec nunc at, tempor tristique nulla. Duis ornare eget nunc id euismod. Nullam efficitur tellus eu mi porttitor, a bibendum nibh elementum. Donec varius ut ligula eget laoreet. Quisque justo mauris, pellentesque eget sagittis sed, posuere sed lectus. Nunc congue urna vel sapien sagittis, vel tincidunt massa interdum. Aliquam lacinia nisl id erat feugiat egestas. Maecenas porta egestas libero, sed vehicula libero placerat nec. 
-`
+`;
 
 export const workplaceBlurb: string = `
     I have been in the industry since August of 2018, the summer after my graduation. I got my start as a Full Stack Software Engineer using React and TypeScript
--- src/pages/AboutWebsite.tsx
+++ src/pages/AboutWebsite.tsx
@@ -6,41 +6,37 @@ import { dummyText } from "../assets/text/text";
 const AboutWebsite: React.FC = () => {
     return (
         <>
-        <Segment vertical style={{ marginTop: "3em" }}>
-            <Grid>
-                <Grid.Row>
-                    <Container textAlign="center">
-                        <Header style={{ fontSize: "2.5em" }}>
-                            <Image src={ReactImage} />
-                            This website was developed with React
-                        </Header>
-                    </Container>
-                </Grid.Row>
-                <Grid.Row>
-                    <Container textAlign="center">
-                        {dummyText}
-                    </Container>
-                </Grid.Row>
-            </Grid>
-        </Segment>
-        <Segment vertical>
-            <Grid>
-                <Grid.Row>
-                    <Container textAlign="center">
-                        <Header style={{ fontSize: "2.5em" }}>
-                            <Image src={ReactImage} />
-                            This website was developed with React
-                        </Header>
-                    </Container>
-                </Grid.Row>
-                <Grid.Row>
-                    <Container textAlign="center">
-                        {dummyText}
-                    </Container>
-                </Grid.Row>
-            </Grid>
-        </Segment>
-    </>
+            <Segment vertical style={{ marginTop: "3em" }}>
+                <Grid>
+                    <Grid.Row>
+                        <Container textAlign="center">
+                            <Header style={{ fontSize: "2.5em" }}>
+                                <Image src={ReactImage} />
+                                This website was developed with React
+                            </Header>
+                        </Container>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Container textAlign="center">{dummyText}</Container>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+            <Segment vertical>
+                <Grid>
+                    <Grid.Row>
+                        <Container textAlign="center">
+                            <Header style={{ fontSize: "2.5em" }}>
+                                <Image src={ReactImage} />
+                                This website was developed with React
+                            </Header>
+                        </Container>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Container textAlign="center">{dummyText}</Container>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+        </>
     );
 };
 
